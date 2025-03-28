c69d500affa67aa344136f380bc7e6f9f6253a5c
Guillermo Tapia <88951508+GuillermoTD@users.noreply.github.com>
2025-03-28 16:50:08 -0400

    se crea funcionalidad para buscar cursos por nombre

--- Services/CourseService.cs
+++ Services/CourseService.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using MongoDB.Bson;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Learn_Managment_System_Backend.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Learn_Managment_System_Backend.Services
 {
@@ -24,15 +25,24 @@ namespace Learn_Managment_System_Backend.Services
             _configuration = configuration;
         }
 
-        public async Task<List<CourseModel>> GetAllCourses()
+        public async Task<List<CourseModel>> GetAllCourses(int pageNumber = 1, int pageSize = 10)
         {
             /*Aqui se utiliza el objeto BsonDocument el cual es el que se encarga de hacer operaciones en los documentos de las colleciones.*/
-            var courses = await Collection.Find(new BsonDocument()).ToListAsync();
+            var courses = await Collection
+                .Find(new BsonDocument())
+                //.Skip((pageNumber - 1) * pageSize)
+                //.Limit(pageSize)
+                .ToListAsync();
+
             return courses;
         }
 
-        public async Task<CourseModel> GetCourseById(string courseId)
+        public async Task<ActionResult<CourseModel>> GetCourseById(string courseId)
         {
+
+
+
+
             /*Validamos que el formato del id es correcto, tomando el string de courseId
             creamos un nuevo objeto de tipo ObjectId con el cual creamos una estructura como esta "id:ObjectId(id)"*/
             if (!ObjectId.TryParse(courseId, out var objectId))
@@ -52,13 +62,23 @@ namespace Learn_Managment_System_Backend.Services
             return query;
         }
 
-        public async Task<CourseModel?> SearchCoursesByTitle(string title)
+        public async Task<List<CourseModel>> SearchCoursesByTitle(string title, int pageSize, int pageNumber)
         {
-            var filter = Builders<CourseModel>.Filter.Eq(c => c.Title, title);
+            if (title == null)
+            {
+                Console.WriteLine("ta vacio bro");
+            }
 
-            var query = await Collection.Find(filter).FirstOrDefaultAsync();
+            Console.WriteLine(title);
+            var filter = Builders<CourseModel>.Filter.Regex("title", new BsonRegularExpression(title, "i"));
 
-            return query;
+            var query = await Collection
+                .Find(filter)
+                .Skip((pageNumber - 1) * pageSize)
+                .Limit(pageSize)
+                .FirstOrDefaultAsync();
+            return [query];
         }
+
     }
 }
